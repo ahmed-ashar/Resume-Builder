7e4147de8df1323879d9ada366b0fb89c20e7e8c
Muhammad Ahmed Ashar <hafizmahmedashar@gmail.com>
2025-05-22 23:39:28 +0500

    wdwa

--- backend/routes/resumeRoutes.js
+++ backend/routes/resumeRoutes.js
@@ -8,6 +8,7 @@ const {
 } = require("../controllers/resumeController");
 const { protect } = require("../middlewares/authMiddleware");
 const { uploadResumeImages } = require("../controllers/uploadImages");
+const upload = require("../middlewares/uploadMiddleware"); // <-- ADD THIS
 
 const router = express.Router();
 
@@ -15,7 +16,15 @@ router.post("/", protect, createResume); // Create Resume
 router.get("/", protect, getUserResume); // Get Resume
 router.get("/:id", protect, getResumeById); // Get Resume By ID
 router.put("/:id", protect, updateResume); // Update Resume
-router.post("/:id/upload-images", protect, uploadResumeImages); // <-- FIXED: POST not PUT
+
+// ADD MULTER MIDDLEWARE HERE:
+router.post(
+  "/:id/upload-images",
+  protect,
+  upload.fields([{ name: "thumbnail" }, { name: "profileImage" }]),
+  uploadResumeImages
+);
+
 router.delete("/:id", protect, deleteResume); // Delete Resume
 
 module.exports = router;
