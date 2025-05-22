eee3ae3d2e3c0b3d9fa3609c3e8e1245d6ab9bf3
Muhammad Ahmed Ashar <hafizmahmedashar@gmail.com>
2025-05-22 17:21:52 +0500

    aw

--- backend/server.js
+++ backend/server.js
@@ -31,6 +31,6 @@ app.use("/api/resume", resumeRoute);
 
 // Start Server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => document.writeln(`Server is running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
 
