452db84fbcd11763a1f25194914a4c5e1cd94cab
Muhammad Ahmed Ashar <hafizmahmedashar@gmail.com>
2025-05-22 20:24:06 +0500

    checking

--- backend/server.js
+++ backend/server.js
@@ -14,22 +14,8 @@ const allowedOrigins = [
   'http://localhost:5173'
 ];
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // allow requests with no origin (like mobile apps, curl, etc.)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      } else {
-        return callback(new Error('Not allowed by CORS'));
-      }
-    },
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+app.use(cors({ origin: true, credentials: true }));
+
 
 // Connect Database
 connectDB();
