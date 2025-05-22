0a05df06a1a9f327ff3e2667bc34d92d1905a2fc
Muhammad Ahmed Ashar <hafizmahmedashar@gmail.com>
2025-05-22 17:13:50 +0500

    aa

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 // Middleware to handle CORS
 app.use(
   cors({
-    origin: "http://localhost:5173" || "*",
+    origin: "http://localhost:5172" || "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
